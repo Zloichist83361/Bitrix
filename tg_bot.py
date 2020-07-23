94a3d127bbf969571e8525e5af3ee155f6dd9ecf
Nikita <62388969+Zloichist83361@users.noreply.github.com>
2020-07-23 13:33:46 +0300

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -1,45 +1,42 @@
 import telebot
 import config
 from telebot import types
+from datetime import datetime
+from bitrix_get_report import create_deal, create_all_sum_deal, create_lead
+from decimal import Decimal
+
+date_create = datetime.now().date()
 
 bot = telebot.TeleBot(config.TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def get_start(message):
-    markup_inline = types.InlineKeyboardMarkup()
-    item_all_reports = types.InlineKeyboardButton(text='Все отчеты', callback_data='all_reports')
-    item_other_reports = types.InlineKeyboardButton(text='Список отчетов', callback_data='other_reports')
+    markup_reply = types.ReplyKeyboardMarkup()
+    item_lead_report = types.KeyboardButton(text='Отчет по лидам')
+    item_deal_report = types.KeyboardButton(text='Отчет по сделкам')
+    item_all_sum_report = types.KeyboardButton(text='Общая сумма продаж')
 
-    markup_inline.add(item_all_reports, item_other_reports)
-    bot.send_message(message.chat.id, 'Отчеты',
-                     reply_markup=markup_inline
-                     )
+    markup_reply.add(item_lead_report, item_deal_report, item_all_sum_report)
+    bot.send_message(message.chat.id, 'ОТЧЕТЫ',
+                     reply_markup=markup_reply)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def answer(call):
-    if call.data == 'all_reports':
-        bot.send_message(call.message.chat.id, 'Тут скоро появится общий отчет')
-    elif call.data == 'other_reports':
-        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item_leads = types.KeyboardButton('Отчет по лидам')
-        item_sales = types.KeyboardButton('Отчет по продажам')
-        item_deals = types.KeyboardButton('Отчет по сделкам')
-
-        markup_reply.add(item_leads, item_sales, item_deals)
-        bot.send_message(call.message.chat.id, 'Выбирете из списка',
-                         reply_markup=markup_reply)
 
 
 @bot.message_handler(content_types=['text'])
 def get_text(message):
     if message.text == 'Отчет по лидам':
-        bot.send_message(message.chat.id, 'Тут скоро появится отчет по лидам')
-    elif message.text == 'Отчет по продажам':
-        bot.send_message(message.chat.id, 'Тут скоро появится отчет по продажам')
+        bot.send_message(message.chat.id, f' Отчет по лидам за {date_create} : \n'
+                                          f'{create_lead()}')
     elif message.text == 'Отчет по сделкам':
-        bot.send_message(message.chat.id, 'Тут скоро появится отчет по сделкам')
+        bot.send_message(message.chat.id, f' Отчет по сделкам за {date_create} : \n'
+                                          f'{create_deal()}')
+    elif message.text == 'Общая сумма продаж':
+        bot.send_message(message.chat.id, f' Общая сумма продаж за {date_create} : \n'
+                                          f'{sum(Decimal(sum_sales) for sum_sales in create_all_sum_deal())} RUB')
 
 
-bot.polling(none_stop=True)
+
+if __name__ == '__main__':
+    bot.infinity_polling()
