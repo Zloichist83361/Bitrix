6233cf8aa7f4053775c0cc545eebe127910287fa
Nikita <62388969+Zloichist83361@users.noreply.github.com>
2020-09-02 10:25:07 +0300

    Update take_FTP.py

--- take_FTP.py
+++ take_FTP.py
@@ -1,10 +1,14 @@
 import ftputil
 import warnings
 import logging
+import sentry_sdk
+import time
+
+sentry_sdk.init("https://9bb20182388f4a05a93b80a15c333130@o419540.ingest.sentry.io/5334036")
 
 logger = logging.getLogger('TAKE_FTP')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler('mylog.log', encoding='utf-8')
+handler = logging.FileHandler('my_log.log', encoding='utf-8')
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 handler.setFormatter(formatter)
@@ -12,29 +16,34 @@ logger.addHandler(handler)
 
 warnings.filterwarnings('ignore')
 
-host ='185.63.60.36'
-username ='anonymous'
-password =''
-port = '8888'
-file = '100-0319.zip'
+host = '109.68.214.235'
+username = 'ikea'
+password = 'Gb23qJ0s1'
+port = '21'
 
-try:
-    ftp_host = ftputil.FTPHost(host, username, password, port)
-    logger.debug(f'Connect to FTP {host}')
-    ftp_host.use_list_a_option = False
-    with ftp_host:
-        list = ftp_host.listdir(ftp_host.curdir)
-        list_file = ''
-        for fname in list:
-            list_file += '\t\t'+ fname + '\n'
-        logger.debug(f'Get file list \n {list_file}')
-        for fname in list:
-            if file == fname:
-                logger.debug(f'File download {fname}')
-                ftp_host.download(fname, 'ftp/' + fname)
-                logger.info(f'Download successfully {fname}')
-            else:
-                logger.error('Unnecessary file')
-except Exception as e:
-    logger.error(f'{e}')
-    raise
+
+def get_last_file_from_ftp():
+    try:
+        ftp_host = ftputil.FTPHost(host, username, password, port)
+        logger.debug(f'Connect to FTP {host}')
+        ftp_host.use_list_a_option = False
+        with ftp_host:
+            list = ftp_host.listdir(ftp_host.curdir)
+            list_file = ''
+            for filename in list:
+                list_file += '\t\t' + filename + '\n'
+            logger.debug(f'Get file list \n {list_file}')
+            for filename in list:
+                if filename[0]:
+                    ftp_host.download(filename, 'Ftp_download/' + filename)
+                    logger.debug(f'File download {filename}')
+                    logger.info(f'Download successfully {filename}')
+                    return filename
+                else:
+                    logger.error('Invalid file')
+    except ConnectionError as e:
+        logger.error(e)
+        sentry_sdk.capture_exception(error=e)
+
+
+time.sleep(300)
