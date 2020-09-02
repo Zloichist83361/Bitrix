aeb8be3b86f2240e7f8c43b25394366b281f02eb
Nikita <62388969+Zloichist83361@users.noreply.github.com>
2020-09-02 10:20:19 +0300

    Update creat_product.py

--- creat_product.py
+++ creat_product.py
@@ -1,88 +1,123 @@
 from bitrix24 import *
 import logging
 import sys
+import sentry_sdk
+import glob
+import os
+
+sentry_sdk.init("https://9bb20182388f4a05a93b80a15c333130@o419540.ingest.sentry.io/5334036")
 
 logger = logging.getLogger('CREAT_PRODUCT')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler('mylog.log', encoding='utf-8')
+handler = logging.FileHandler('my_log.log', encoding='utf-8')
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 products = []
-
 delimiter = ';'
-try:
-    with open('te1.csv', encoding='utf-8') as f:
-        for line in f:
-            product = line.split(delimiter)
-            products.append(product)
-except Exception as e:
-    logger.error(f'{e}')
-    sys.exit('Stop')
 
-if len(products) < 2:
-    logger.info('No correct list')
-    sys.exit('Stop')
 
-if len(products[0]) == 13:
-    logger.error('No correct size product')
-    sys.exit('Stop')
-
-del products[0]
-
-for product in products:
-    id = product[0]
+def check_section(product):
     section = product[2]
-    section_bitrix_id = ''
-    bx24 = Bitrix24('https://b24-jymdkh.bitrix24.ru/rest/1/hu3iszfs1ozy6qwc/')
-    if section:
-        result = bx24.callMethod('crm.productsection.list', filter={'NAME': section}, select=['ID'])
-        logger.debug(f'List product section {result}')
+    bx24 = Bitrix24('https://b24-3xzo32.bitrix24.ru/rest/1/szrks03x762pgphu/profile/')
+    section_id_bitrix = bx24.callMethod('crm.productsection.list', filter={'NAME': section}, select=['ID'])
+    logger.debug(f'List product section {section_id_bitrix}')
+    return section_id_bitrix
 
-        if result and result[0]:
-            section_bitrix_id = result[0].get('ID')
-        else:
-            section_bitrix_id = bx24.callMethod('crm.productsection.add', fields={'NAME': section})
-            logger.debug(f'{section} Add product section')
 
-    result = bx24.callMethod('crm.product.list', filter={'ID': id}, select=['ID'])
+def check_product(product):
+    id = product[0]
+    bx24 = Bitrix24('https://b24-3xzo32.bitrix24.ru/rest/1/szrks03x762pgphu/profile/')
+    result = bx24.callMethod('crm.product.list', filter={'PROPERTY_125': id}, select=['ID'])
     logger.debug(f'List product {result}')
+    return result
 
-    if result and result[0]:
-        update = {'NAME': product[3],
-                  'PRICE': product[7],
-                  'PROPERTY_137': product[1],
-                  'PROPERTY_141': product[4],
-                  'PROPERTY_143': product[5],
-                  'PROPERTY_145': product[6],
-                  'PROPERTY_149': product[9],
-                  'PROPERTY_151': product[10],
-                  'PROPERTY_153': product[11],
-                  'PROPERTY_155': product[12],
-                  'SECTION_ID': section_bitrix_id
-                  }
-        result = bx24.callMethod('crm.product.update', id=id, fields=update)
-        logger.info(f'Update product {product[3], product[1]}')
-        logger.debug(f'{product[2], product[4], product[5], product[6], product[7], product[8], product[9], product[10], product[11], product[12]}')
 
-    else:
-        add = {'ID': id,
-               'NAME': product[3],
-               'CURRENCY_ID': 'RUB',
-               'PRICE': product[7],
-               'PROPERTY_137': product[1],
-               'PROPERTY_141': product[4],
-               'PROPERTY_143': product[5],
-               'PROPERTY_145': product[6],
-               'PROPERTY_149': product[9],
-               'PROPERTY_151': product[10],
-               'PROPERTY_153': product[11],
-               'PROPERTY_155': product[12],
-               'SECTION_ID': section_bitrix_id
-               }
-        result = bx24.callMethod('crm.product.add', fields=add)
-        logger.info(f'Add product {product[3], product[1]}')
-        logger.debug(f'{product[2], product[4], product[5], product[6], product[7], product[8], product[9], product[10], product[11], product[12]}')
+def create_section(product):
+    section = product[2]
+    bx24 = Bitrix24('https://b24-3xzo32.bitrix24.ru/rest/1/szrks03x762pgphu/profile/')
+    section_bitrix = bx24.callMethod('crm.productsection.add', fields={'NAME': section})
+    logger.debug(f'Add product section {section}')
+    return section_bitrix
 
+
+def update_product(product, section_id, product_id):
+    bx24 = Bitrix24('https://b24-3xzo32.bitrix24.ru/rest/1/szrks03x762pgphu/profile/')
+    result = bx24.callMethod('crm.product.update', id=product_id, fields={'NAME': product[3],
+                                                                          'PRICE': product[7],
+                                                                          'PROPERTY_103': product[1],  # Штрихкод
+                                                                          'PROPERTY_105': product[2],
+                                                                          # Группа товаров
+                                                                          'PROPERTY_107': product[4],  # Размер
+                                                                          'PROPERTY_109': product[5],  # Цвет
+                                                                          'PROPERTY_111': product[6],  # Остаток
+                                                                          'PROPERTY_113': product[9],  # Артикул
+                                                                          'PROPERTY_115': product[10],
+                                                                          # Страна-производитель
+                                                                          'PROPERTY_117': product[11],  # Код товара
+                                                                          'PROPERTY_119': product[12],  # Атрибут4
+                                                                          'SECTION_ID': section_id
+                                                                          })
+    logger.info(f'Update product {product[3], product[1]}')
+    logger.debug(
+        f'{product[2], product[4], product[5], product[6], product[7], product[8], product[9], product[10], product[11], product[12]}')
+    return result
+
+
+def create_product(product, section_id):
+    id = product[0]
+    bx24 = Bitrix24('https://b24-3xzo32.bitrix24.ru/rest/1/szrks03x762pgphu/')
+    result = bx24.callMethod('crm.product.add', fields={'NAME': product[3],
+                                                        'CURRENCY_ID': 'RUB',
+                                                        'PRICE': product[7],
+                                                        'PROPERTY_103': product[1],  # Штрихкод
+                                                        'PROPERTY_105': product[2],  # Группа товаров
+                                                        'PROPERTY_107': product[4],  # Размер
+                                                        'PROPERTY_109': product[5],  # Цвет
+                                                        'PROPERTY_111': product[6],  # Остаток
+                                                        'PROPERTY_113': product[9],  # Артикул
+                                                        'PROPERTY_115': product[10],
+                                                        # Страна-производитель
+                                                        'PROPERTY_117': product[11],  # Код товара
+                                                        'PROPERTY_119': product[12],  # Атрибут4
+                                                        'SECTION_ID': section_id,
+                                                        'PROPERTY_125': id  # ID csv
+                                                        })
+    logger.info(f'Add product {product[3], product[1]}')
+    logger.debug(
+        f'{product[2], product[4], product[5], product[6], product[7], product[8], product[9], product[10], product[11], product[12]}')
+    return result
+
+
+def create_products_from_csv():
+    try:
+        files_path = os.path.join('Ftp_download', '*')
+        files = sorted(glob.iglob(files_path), key=os.path.getctime, reverse=True)
+        with open(files[0], encoding='utf-8') as f:
+            for line in f:
+                product = line.split(delimiter)
+                products.append(product)
+                if len(products) == 13:
+                    logger.info('No correct list')
+                    sys.exit('Stop')
+                for product in products[1:]:
+                    section_bitrix = check_section(product)
+                    if not section_bitrix:
+                        section_id = create_section(product)
+                        logger.info(f'Not section_id{section_id}')
+                    else:
+                        section_id = section_bitrix[0].get('ID')
+                        logger.info(f'Yes section_id{section_id}')
+                    bitrix = check_product(product)
+                    if not bitrix:
+                        create_product(product, section_id)
+                    else:
+                        update_product(product, section_id, bitrix[0]['ID'])
+    except FileNotFoundError as e:
+        logger.error(e)
+        sentry_sdk.capture_exception(error=e)
+    except OSError as e:
+        logger.error(e)
+        sentry_sdk.capture_exception(error=e)
